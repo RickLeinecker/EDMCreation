8d3644bd2185a2679d889d732b2d6603ec269b7a
Treven Gonzalez <treven@knights.ucf.edu>
2021-03-28 10:55:56 -0400

    search in progress

--- EDMCreationWeb/backend/routes/compositions.js
+++ EDMCreationWeb/backend/routes/compositions.js
@@ -134,31 +134,31 @@ router.route('/upload').post(auth, parser.single("file"), auth, [
 //     //need to redirect on page once done? 
 // });
 
-//single play and update count for when a user hits play on a given song
-router.route('/play/:filename').get((req, res) => {
-    gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
-        // Check if file
-        if (!file || file.length === 0) {
-            return res.status(404).json({ err: 'No file exists' });
-        }
+// //single play and update count for when a user hits play on a given song
+// router.route('/play/:filename').get((req, res) => {
+//     gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+//         // Check if file
+//         if (!file || file.length === 0) {
+//             return res.status(404).json({ err: 'No file exists' });
+//         }
 
-        // File exists
+//         // File exists
 
-        // Check if midi then load
-        if (file.contentType === 'audio/mid') {
+//         // Check if midi then load
+//         if (file.contentType === 'audio/mid') {
 
-        } else {
-            return res.status(404).json({ err: 'Not a MID file' });
-        }
-        const readstream = gfs.createReadStream(file.filename);
+//         } else {
+//             return res.status(404).json({ err: 'Not a MID file' });
+//         }
+//         const readstream = gfs.createReadStream(file.filename);
 
-        //update listens for comp and user and send file data
-        User.updateOne({ "compositions.filename": req.params.filename }, { $inc: { "compositions.$.listens": 1, listens_count: 1 } })
-            .then(() => readstream.pipe(res)) //Return File for playing
-            .catch(err => res.status(400).json('Error: ' + err)); //might need to remove uploaded song from database if error occured
+//         //update listens for comp and user and send file data
+//         User.updateOne({ "compositions.filename": req.params.filename }, { $inc: { "compositions.$.listens": 1, listens_count: 1 } })
+//             .then(() => readstream.pipe(res)) //Return File for playing
+//             .catch(err => res.status(400).json('Error: ' + err)); //might need to remove uploaded song from database if error occured
 
-    });
-});
+//     });
+// });
 
 //load multiple composition infos for pages
 //REQUIRES PAGE NUMBER IN ROUTE, STARTING ON 1
@@ -247,6 +247,7 @@ router.route('/popular').get(async (req, res) => {
     }
 });
 
+//for a given profile
 router.route('/user/:username').get(async (req, res) => {
     const songsPerPage = 5;
     const skip = songsPerPage * (req.query.page - 1);
@@ -406,6 +407,7 @@ router.route('/editsave').post(auth, [
 
     });
 
+
 router.route('/incrementplaycount').post((req, res) => {
          
           User.updateOne({ "compositions._id": req.body.song_id }, { $inc: { "compositions.$.listens": 1, listens_count: 1 } })
@@ -413,5 +415,95 @@ router.route('/incrementplaycount').post((req, res) => {
           .catch(err => res.status(400).json('Error: ' + err)); 
 
 });
+
+
+
+// router.route('/search').get(async (req, res) => {
+//     const songsPerPage = 5;
+//     const skip = songsPerPage * (req.query.page - 1);
+//     const lookup = req.query.search;
+
+//     try {
+//         const songs = await User.aggregate([
+//             { $match: { username: { $regex: new RegExp(req.params.username, "i") } } },
+//             {
+//                 $unwind:
+//                 {
+//                     path: "$compositions",
+//                     preserveNullAndEmptyArrays: false
+//                 }
+//             },
+//             {
+//                 $project: {
+//                     composition_id: "$compositions._id",
+//                     title: "$compositions.title",
+//                     username: "$username",
+//                     num_comments: { $size: "$compositions.comments" },
+//                     date: "$compositions.last_modified",
+//                     comments: "$compositions.comments",
+//                     genre: "$compositions.genre",
+//                     path: "$compositions.path",
+//                     listens: "$compositions.listens"
+//                 },
+//             },
+//             {
+//                 $unwind:
+//                 {
+//                     path: "$comments",
+//                     preserveNullAndEmptyArrays: true
+//                 }
+//             },
+//             { $sort: { "comments.last_modified": -1 } },
+//             {
+//                 $group: {
+//                     _id: "$composition_id",
+//                     composition_id: { $first: "$composition_id" },
+//                     user_id: { $first: "$_id" },
+//                     title: { $first: "$title" },
+//                     username: { $first: "$username" },
+//                     num_comments: { $first: "$num_comments" },
+//                     date: { $first: "$date" },
+//                     comments: { $push: "$comments" },
+//                     genre: { $first: "$genre" },
+//                     path: { $first: "$path" },
+//                     listens: { $first: "$listens" },
+//                 }
+//             },
+//             {
+//                 $lookup: {
+//                     from: "users",
+//                     localField: "_id",
+//                     foreignField: "favorites.composition_id",
+//                     as: "favorites",
+//                 }
+//             },
+//             {
+//                 $project: {
+//                     composition_id: 1,
+//                     user_id: 1,
+//                     title: 1,
+//                     username: 1,
+//                     num_comments: 1,
+//                     date: 1,
+//                     comments: 1,
+//                     genre: 1,
+//                     path: 1,
+//                     listens: 1,
+//                     likes: { $size: "$favorites" }
+//                 }
+//             },
+//             { $sort: { "_id": 1, "listens": -1 } }, //descending values for listens
+//             { $skip: skip },
+//             { $limit: songsPerPage }, //skip controls page number and limit controls output
+//         ]);
+
+//         if (!songs) throw Error('No items');
+
+//         res.status(200).json(songs);
+//     } catch (e) {
+//         res.status(400).json({ msg: e.message });
+//     }
+// });
+
 
 module.exports = router;
